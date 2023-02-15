8ff342dc052384c04f22af689bcf89471851eba2
Amirkhan <amirhan9878@gmail.com>
2023-02-15 10:48:14 +0600

    Comments added

--- controllers/OrderController.js
+++ controllers/OrderController.js
@@ -46,6 +46,49 @@ exports.deleteOrder = async (req, res) => {
   }
 };
 
+
+//Need to complete 
+//this func will be for kaspi side
+//req that come from kaspi will be processed here
+//when command is 'check'
+//  1)take req.query.account and check do we have it in our database ('account' in our database is 'id')
+//  2)if have it in our database compare req.query.sum and sum in our database
+//  2.1) if two sum is equal return json response
+//{
+//  txn_id: req.query.txn_id,
+//  result: 0,
+//  comment: ""
+//}
+//  2.2)if two sum is not equal return json response
+//{
+//  txn_id: req.query.txn_id,
+//  result: 5,
+//  comment: ""
+//}
+//  3)if don't have it in our database return json response
+//{
+//  txn_id: req.query.txn_id,
+//  result: 1,
+//  comment: ""
+//}
+//when command is 'pay'
+//  1)take req.query.account and check do we have it in our database ('account' in our database is 'id')
+//  2)if have it in our database compare req.query.sum and sum in our database
+//{
+//  txn_id: req.query.txn_id,
+//  prv_txn_id: ???,
+//  result: 0,
+//  sum: req.query.sum,
+//  comment: ""
+//}
+//  3)if don't have it in our database return json response
+//{
+//  txn_id: req.query.txn_id,
+//  prv_txn_id: ???,
+//  result: 1,
+//  sum: req.query.sum,
+//  comment: ""
+//}
 exports.checkOrderById = async (req, res) => {
   try {
     const order = await orderService.getOrderById(req.query.account);
