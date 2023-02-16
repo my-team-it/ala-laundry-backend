d8927106e6fd2ee5ff8a343d6dd42a2cd8cf3653
Amirkhan <amirhan9878@gmail.com>
2023-02-16 20:47:46 +0600

    Service for Transaction added

--- models/Transaction.js
+++ models/Transaction.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const order_schema = new Schema({
+const transaction_schema = new Schema({
     prv_txn_id: Number
  });
   
-module.exports = mongoose.model("Order", order_schema);
+module.exports = mongoose.model("Transaction", transaction_schema);
---
+++ services/TransactionService.js
@@ -0,0 +1,25 @@
+const TransactionModel = require("../models/Transaction");
+ 
+exports.getAllTransaction = async () => {
+  return await TransactionModel.find();
+};
+ 
+exports.createTransaction = async (Transaction) => {
+  return await TransactionModel.create(Transaction);
+};
+
+exports.getTransactionById = async (id) => {
+  return await TransactionModel.findById(id);
+};
+
+exports.getTransactionByPrvTxnId = async (prv_txn_id) => {
+  return await TransactionModel.findOne({prv_txn_id:prv_txn_id});
+};
+ 
+exports.updateTransaction = async (id, Transaction) => {
+  return await TransactionModel.findByIdAndUpdate(id, Transaction);
+};
+ 
+exports.deleteTransaction = async (id) => {
+  return await TransactionModel.findByIdAndDelete(id);
+};
