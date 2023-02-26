9f487250260020d4bab3c28792d0964209aa139a
Amirkhan <amirhan9878@gmail.com>
2023-02-26 21:35:42 +0600

    sefaefaef

--- controllers/OrderController.js
+++ controllers/OrderController.js
@@ -97,6 +97,7 @@ exports.checkOrderById = async (req, res) => {
     res.json(json);
 
   } catch (err) {
+    console.log(err)
     res.json({ txn_id: req.query.txn_id, result: 1, comment: "Error during processing" });
   }
 }
