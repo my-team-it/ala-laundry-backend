5e92a0f4d71b6405aecbb93977d00ed1a1b5f796
Amirkhan <amirhan9878@gmail.com>
2023-02-27 16:26:54 +0600

    awfdawd

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 git pull
-docker build --build-arg MONGODB_URI_ARG=${DATABASE_URL} -t ala-laundry-backend:latest .
+docker build --build-arg MONGODB_URI_ARG=${MONGO_DATABASE_URL} -t ala-laundry-backend:latest .
 docker stop ala-laundry-backend
 docker rm ala-laundry-backend
 docker run -p 443:443 -d --name ala-laundry-backend ala-laundry-backend:latest
