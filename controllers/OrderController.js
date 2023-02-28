5084e8c2e012e918a310bc82d72f9490463a3c44
Amirkhan <amirhan9878@gmail.com>
2023-02-28 17:10:29 +0600

    aefawda

--- controllers/OrderController.js
+++ controllers/OrderController.js
@@ -17,7 +17,7 @@ function check(sumInDatabase, txn_id, sum) {
     return {txn_id:txn_id, result: 0, sum:sum, comment: "Item found"};
   }
 
-  return {txt_id:txn_id, result: 5, comment: "Item sum incorrect"};
+  return {txt_id:txn_id, result: 5, sum:sum, comment: "Item sum incorrect"};
 }
 
 async function pay(query, sumInDatabase) {
