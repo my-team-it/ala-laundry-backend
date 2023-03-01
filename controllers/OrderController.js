c033db6d30ddb51c1e9f78b34cd24fce7676eaf7
Amirkhan <amirhan9878@gmail.com>
2023-03-01 16:02:47 +0600

    awdawdaw

--- controllers/OrderController.js
+++ controllers/OrderController.js
@@ -21,13 +21,14 @@ async function check(query) {
 async function pay(query, sumInDatabase) {
   const order = await orderService.getOrderById(query.account);
   const prv_txn_id = generate_id();
+  
   if (order.payment_status == 'paid') {
     return { txn_id:query.txn_id, prv_txn_id: prv_txn_id, result: 3, sum:parseInt(query.sum), bin:"030213500928", comment: "Item already paid" };
   }
 
   if (sumInDatabase == query.sum) {
     const updateOrderResult = await orderService.updateOrder(query.account, {payment_status:"paid", machine_status:1});
-    order = await orderService.getOrderById(query.account);
+    const order = await orderService.getOrderById(query.account);
     const result = await firebaseService.writeData(order, order.machine_id);
     return { txn_id:query.txn_id, prv_txn_id: prv_txn_id, result: 0, sum:parseInt(query.sum), bin:"030213500928", comment: "Pay item found" };
   }
