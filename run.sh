c438ee0aa9f510db48741f90d35b502b15193053
Nurdaulet Akhmatov <70795559+nurdashymside@users.noreply.github.com>
2023-03-02 21:50:42 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@ git pull
 docker build --build-arg MONGODB_URI_ARG=${MONGO_DATABASE_URL} -t ala-laundry-backend:latest .
 docker stop ala-laundry-backend
 docker rm ala-laundry-backend
-docker run -p 443:443 -d --name ala-laundry-backend ala-laundry-backend:latest
+docker run -p 443:443 -p 80:80 -d --name ala-laundry-backend ala-laundry-backend:latest
