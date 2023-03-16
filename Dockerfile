ffc77f76197008602d407b6d0c02036fd996241d
Amirkhan <amirhan9878@gmail.com>
2023-03-17 02:08:13 +0600

    awfawfawd

--- Dockerfile
+++ Dockerfile
@@ -12,8 +12,6 @@ RUN npm install
 # If you are building your code for production
 # RUN npm ci --only=production
 
-ENV MONGODB_ENV=''
-
 # Bundle app source
 COPY . .
 
--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@ echo ${MONGO_DATABASE_URL}
 sudo docker build -t ala-laundry-backend:latest .
 docker stop ala-laundry-backend
 docker rm ala-laundry-backend
-docker run -p 4343:4343 -p 8080:8080 -e MONGODB_ENV=${MONGO_DATABASE_URL} -d --name ala-laundry-backend ala-laundry-backend:latest
+docker run -p 4343:4343 -p 8080:8080 -e MONGO_DATABASE_URL=${MONGO_DATABASE_URL} -d --name ala-laundry-backend ala-laundry-backend:latest
