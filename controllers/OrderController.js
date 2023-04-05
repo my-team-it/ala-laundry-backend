974b99d6fbf6d8922ce81b95bcac4e8b72245e19
Amirkhan <amirhan9878@gmail.com>
2023-04-05 08:22:41 +0600

    aefawd

--- controllers/OrderController.js
+++ controllers/OrderController.js
@@ -58,10 +58,10 @@ async function pay(query) {
   if (list_of_prices[service_id] == orderJson.sum) {
     const order = await orderService.updateOrder(id, orderJson);
     console.log(dateTime.getDateTime() + "| Update order:" + order);
-    const result = await firebaseService.writeData({machine_status:1,mode: service_id}, id);
+    const result = await firebaseService.writeData({machine_status:1,mode: service_id}, order.machine_id);
     setTimeout(async () => {
       const unpaidOrder = await orderService.updateOrder(id, {machine_status:0, payment_status:"unpaid"});
-      const result = await firebaseService.writeData({machine_status:0,mode: 0}, id);
+      const result = await firebaseService.writeData({machine_status:0,mode: 0}, unpaidOrder.machine_id);
     }, 60 * 1000)
     return { txn_id:query.txn_id, prv_txn_id: prv_txn_id, result: 0, sum:parseInt(query.sum), bin:'030213500928', comment: 'Pay item found'};
   }
