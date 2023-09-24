fd776fc19cf724f4dadfdd8cc1c2c80d9e28b59c
Amirkhan <amirhan9878@gmail.com>
2023-09-25 03:44:55 +0600

    gseadfdvsbfgdafs

--- src/controllers/kaspiController.js
+++ src/controllers/kaspiController.js
@@ -16,7 +16,9 @@ function stopInterval(machineId) {
 }
 
 async function processWashing(washing_id) {
-  const washing = await washingService.readWashing(washing_id);
+  const washing = await washingService.readLastWashingStateByMachineID(
+    washing_id
+  );
   console.log(washing_id);
   console.log(washing);
   if (washing[0].state === "ACTIVE") {
