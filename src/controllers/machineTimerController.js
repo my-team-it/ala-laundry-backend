15be68bf994180c1d5213431a1e61edd8d8b50a7
Amirkhan <amirhan9878@gmail.com>
2023-09-24 22:29:59 +0600

    gadfasd

--- src/controllers/machineTimerController.js
+++ src/controllers/machineTimerController.js
@@ -1,12 +1,8 @@
-import firebaseService from "../services/firebaseService.js";
 import machineTimerService from "../services/machineTimerService.js";
 
 export const readMachineTimers = async (req, res) => {
   res.header("Access-Control-Allow-Origin", "*");
   const [rows] = await machineTimerService.readMachineTimers();
-  for (let i = 0; i < rows[0].length; i++) {
-    firebaseService.onTimerChange(rows[0][i].machine_id);
-  }
   res.json({ data: rows });
 };
 
