ee16dc5a193c65e2b4347950ecf2bdf6d1aac1e9
Amirkhan <amirhan9878@gmail.com>
2023-09-25 03:38:51 +0600

    sfgasdaws

--- src/controllers/kaspiController.js
+++ src/controllers/kaspiController.js
@@ -16,7 +16,7 @@ function stopInterval(machineId) {
 }
 
 async function processWashing(washing_id) {
-  const [washing] = await washingService.readWashing(washing_id);
+  const washing = await washingService.readWashing(washing_id);
   console.log(washing);
   if (washing[0].state === "ACTIVE") {
     const isDoorOpenList = [];
