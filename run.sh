3282e3316225b6a68efef4d15dca7197698c7c93
Amirkhan <amirhan9878@gmail.com>
2023-10-15 20:23:36 +0600

    Complete run sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 docker pull realsleep/ala-laundry-backend:latest
 docker stop ala-laundry-backend
 docker rm ala-laundry-backend
-docker run -p 443:443 -p 80:80 -e MONGO_DATABASE_URL=${MONGO_DATABASE_URL} -d --name ala-laundry-backend realsleep/ala-laundry-backend:latest
+docker run -p 443:443 -p 80:80 -d --name ala-laundry-backend realsleep/ala-laundry-backend:latest
