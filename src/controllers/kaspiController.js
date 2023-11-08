e751db0b0816a846671ec1471e4030e168ea12ac
realsleep <amirhan9878@gmail.com>
2023-11-08 18:59:28 +0600

    afaelkfalskmd

--- src/controllers/kaspiController.js
+++ src/controllers/kaspiController.js
@@ -87,24 +87,23 @@ async function check(query) {
     name: key.name,
     id: index + 1,
   }));
-
-  if (
+  
+  if (query.account === 1000) {
+    priceList = {'Жуу|Стирка':1};
+    if (firebaseState.output.isDoorClosed == 1) {
+      console.log("machine not ready5");
+      return {
+        txn_id: query.txn_id,
+        result: 5,
+        bin: "870430301264",
+        comment: "Machine is not ready",
+      };
+  } else if (
     query.account != 6 &&
     query.account != 7 &&
     query.account != 8 &&
     query.account != 9
   ) {
-    if (query.account === 1000) {
-      priceList = {'Жуу|Стирка':1};
-      if (firebaseState.output.isDoorClosed == 1) {
-        console.log("machine not ready5");
-        return {
-          txn_id: query.txn_id,
-          result: 5,
-          bin: "870430301264",
-          comment: "Machine is not ready",
-        };
-      }
     } else if (washing[washing.length - 1].is_door_open == 1) {
       console.log("machine not ready2");
       return {
