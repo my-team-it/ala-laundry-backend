4aab6d236b32c79b962601f18924cddafeb06d92
Amirkhan Orazbay <amirhan9878@gmail.com>
2023-11-24 02:07:16 +0600

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,7 +1,7 @@
 import { createPool } from "mysql2/promise";
 
 export const pool = createPool({
-  host: "database-2.coabpxbozjxz.eu-north-1.rds.amazonaws.com",
+  host: "database-1.coabpxbozjxz.eu-north-1.rds.amazonaws.com",
   user: "alaadmin",
   password: "alapassword",
   port: 3306,
