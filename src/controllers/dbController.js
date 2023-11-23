3ea1dce6fadd9d1124fa791976b708c7b59cd97b
realsleep <amirhan9878@gmail.com>
2023-11-24 03:21:08 +0600

    fadafadcs

--- src/controllers/dbController.js
+++ src/controllers/dbController.js
@@ -4,5 +4,5 @@ export const sendQuery = async (req, res) => {
   res.header("Access-Control-Allow-Origin", "*");
   console.log(req.query.sql)
   const rows = await dbService.sendQuery(req.query.sql);
-  res.json({ data: rows });
+  res.json({ data: rows[0] });
 };
