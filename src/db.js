9510e67f205f8982b740974f43f93b9d9f8c7b48
realsleep <amirhan9878@gmail.com>
2024-01-20 15:50:20 +0600

    update db url

--- src/db.js
+++ src/db.js
@@ -1,9 +1,9 @@
 import { createPool } from "mysql2/promise";
 
 export const pool = createPool({
-  host: "database-1.coabpxbozjxz.eu-north-1.rds.amazonaws.com",
-  user: "admin",
-  password: "adminAla!",
+  host: "restored-db.coabpxbozjxz.eu-north-1.rds.amazonaws.com",
+  user: "alaadmin",
+  password: "alapassword",
   port: 3306,
   database: "alaprod",
 });
