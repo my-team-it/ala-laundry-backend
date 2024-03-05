7224834cfebb49c959efcdef1054285effc49298
realsleep <amirhan9878@gmail.com>
2024-03-05 22:06:15 +0500

    add await to avoid pending objects

--- src/test.js
+++ src/test.js
@@ -1,7 +1,7 @@
 import washingService from "./services/washingService.js";
 
 async function test(washing_id) {
-    const isDoorOpenList1 = washingService.readIsDoorOpenStatesByID(washing_id);
+    const isDoorOpenList1 = await washingService.readIsDoorOpenStatesByID(washing_id);
     console.log(isDoorOpenList1);
     const isDoorClosedOnAllChecks1 = isDoorOpenList1.every(status => !status);
     console.log(isDoorClosedOnAllChecks1);
@@ -9,19 +9,19 @@ async function test(washing_id) {
     const key1 = "is_door_open_"+1;
     const value1 = 1;
     console.log({key1: value1});
-    washingService.updateIsDoorOpenByID(washing_id, {[key1]: value1});
+    await washingService.updateIsDoorOpenByID(washing_id, {[key1]: value1});
 
     const key2 = "is_door_open_"+2;
     const value2 = 0;
     console.log({key2: value2});
-    washingService.updateIsDoorOpenByID(washing_id, {[key2]: value2});
+    await washingService.updateIsDoorOpenByID(washing_id, {[key2]: value2});
     
     const key3 = "is_door_open_"+3;
     const value3 = 1;
     console.log({key3: value3});
-    washingService.updateIsDoorOpenByID(washing_id, {[key3]: value3});
+    await washingService.updateIsDoorOpenByID(washing_id, {[key3]: value3});
 
-    const isDoorOpenList = washingService.readIsDoorOpenStatesByID(washing_id);
+    const isDoorOpenList = await washingService.readIsDoorOpenStatesByID(washing_id);
     console.log(isDoorOpenList);
     const isDoorClosedOnAllChecks = isDoorOpenList.every(status => !status);
     console.log(isDoorClosedOnAllChecks);
