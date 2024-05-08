755af4f6687cd639c995f25cdbc0b0b2e69dedab
amirkhanorazbay <142108477+amirkhanorazbay@users.noreply.github.com>
2024-05-08 10:34:26 +0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@ docker pull realsleep/ala-laundry-backend-test:latest
 docker stop ala-laundry-backend
 docker rm ala-laundry-backend
 docker image prune
-docker run -p 443:443 -p 80:80 -d --name ala-laundry-backend realsleep/ala-laundry-backend-test:latest
+docker run -p 443:443 -p 80:80 --restart always -d --name ala-laundry-backend realsleep/ala-laundry-backend-test:latest
