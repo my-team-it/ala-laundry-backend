b976397163ed65819779db772637fe3aecb1fc4d
Amirkhan Orazbay <amirkhan.orazbay@qcloudy.io>
2024-05-23 13:49:39 +0500

    Update new db

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const HTTP_PORT = process.env.HTTP_PORT || 80;
 export const HTTPS_PORT = process.env.HTTPS_PORT || 443;
 
-export const DB_HOST = "ala-laundry-database-prod.czwiyugwum02.eu-north-1.rds.amazonaws.com";
+export const DB_HOST = "alal-db-prod-23.czwiyugwum02.eu-north-1.rds.amazonaws.com";
 export const DB_USER = "alaadmin";
 export const DB_PASSWORD = "alapassword";
 export const DB_PORT = 3306;
