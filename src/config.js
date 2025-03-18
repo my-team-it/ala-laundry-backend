23842ef37faf82b512198b273a1b55868ee6ef08
Nurdaulet Akhmatov <70795559+nurdamiron@users.noreply.github.com>
2025-03-18 22:40:36 +0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 export const HTTP_PORT = process.env.HTTP_PORT || 80;
 export const HTTPS_PORT = process.env.HTTPS_PORT || 443;
 
-export const DB_HOST = "alal-db-prod-23.czwiyugwum02.eu-north-1.rds.amazonaws.com";
-export const DB_USER = "alaadmin";
-export const DB_PASSWORD = "alapassword";
+export const DB_HOST = "biz360.czwiyugwum02.eu-north-1.rds.amazonaws.com";
+export const DB_USER = "root";
+export const DB_PASSWORD = "nurda0101";
 export const DB_PORT = 3306;
 export const DB_NAME = "alaprod";
 
